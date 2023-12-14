a7ee77fc949eb22df576ba79ab8bbf0098b91748
Naufal Farras <farrasn7@gmail.com>
2023-12-14 17:54:52 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+  createBrowserRouter,
+  createHashRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import Signup from './screens/Signup';
+import SignIn from './screens/SignIn';
+import LoginHelp from './screens/LoginHelp';
+import './App.css';
+
+const router = createHashRouter([
+  {
+    path: "/pk",
+    element: <Signup />,
+  },
+  {
+    path: "/",
+    element: <Navigate to="/pk" replace />,
+  },
+  {
+    path: "/pk/login",
+    element: <SignIn />,
+  },
+  {
+    path: "/pk/LoginHelp",
+    element: <LoginHelp />,
+  },
+  {
+    path: "*",
+    element: <Navigate to="/" replace />
+  },
+]);
+
+function App() {
+  return (
+    <>
+       <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
